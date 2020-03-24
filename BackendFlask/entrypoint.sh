a86c9eb11891fd0506d6b1d0f8179ced99ed98c9
shiroyk <kumoocat@gmail.com>
2020-03-24 21:05:51 +0800

    Add run migration

--- BackendFlask/entrypoint.sh
+++ BackendFlask/entrypoint.sh
@@ -8,4 +8,8 @@ while ! nc -z redis 6379; do sleep 3; done
 
 echo "App can connect redis!"
 
+sleep 1 && echo "Run migrations"
+
+/usr/local/bin/flask db upgrade head
+
 /usr/local/bin/gunicorn -c config/gunicorn.py lightword:app --log-config config/logging.conf
