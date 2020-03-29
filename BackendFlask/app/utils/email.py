12dc02cb5508dc366c9c3b2985f33a579498125f
shiroyk <kumoocat@gmail.com>
2020-03-29 17:06:47 +0800

    Improved generate verification code

--- BackendFlask/app/utils/email.py
+++ BackendFlask/app/utils/email.py
@@ -1,33 +1,35 @@
 import random, threading
 
-from flask import copy_current_request_context
+from flask import copy_current_request_context, current_app
 from flask_mail import Message
 from app import mail
 
 def generate_code(code = []):
-    random_num = random.randint(0, 9)
-    random_upper = chr(random.randint(65, 90))
-    random_lower = chr(random.randint(97, 122))
-    code.append(str(random_num))
-    code.append(random_upper)
-    code.append(random_lower)
-    if len(code) != 6:
-        generate_code(code)
-        return ''.join(code)
+    for i in range(1,10):
+        num = random.randint(0, 9)
+        upper = chr(random.randint(65, 90))
+        lower = chr(random.randint(97, 122))
+        code.append(str(num))
+        code.append(upper)
+        code.append(lower)
+    return ''.join(code)[num:num+6]
 
 def send_email(subject, sender, recipients, msg_body, msg_html,
                **kwargs):
-
     message = Message(subject,
                       sender=sender,
                       recipients=recipients,
                       **kwargs)
     message.body = msg_body
     message.html = msg_html
-    @copy_current_request_context
-    def send_message(message):
-        mail.send(message)
 
+    @copy_current_request_context
+    def send_message(message,):
+        try:
+            mail.send(message)
+        except Exception as e:
+            current_app.logger.error('%s: Send email failed, please check config', __name__)
+    
     sender = threading.Thread(target=send_message, args=(message,))
     sender.start()
     return sender
